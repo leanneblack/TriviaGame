d5b1c5c5b941845ce137b97e0591835f61c06202
leanneblack <leanne.black@cox.net>
2019-02-23 14:26:35 -0800

    attempted assignemnt

---
+++ assets/javascript/app.js
@@ -0,0 +1,111 @@
+
+$( document ).ready(function() {
+
+// create trivia questions 
+
+var trivaQuestions = [
+    {
+        question: "What is Ron’s favorite whiskey?",
+        choices: ["Jameson", "Jack Daniels", "Lagavulin", "Glenlivet"],
+        correctChoice: 2
+    },
+
+    {
+        question: "What month and day was Jerry born?",
+        choices: [ "Feburary 29th", "April 1st", "April 20th", "WHY WAS HE EVEN BORN?!?" ],
+        correctChoice: 0
+    },
+
+    {
+        question: "Who is Chris Traeger's Therapist?",
+        choices: ["Dr. Lu Saperstein", "Dr. Richard Nygard", "Dr. Harris", "Dr. Nick Newport"],
+        correctChoice: 1
+    },
+
+    {
+        question: "What country is Pawnee's sister city located in?",
+        choices: ["France", "England", "Canada", "Venezuela"],
+        correctChoice: 3
+    },
+
+    { 
+        question: "What car make did Jean Ralphio get hit by to 'make money the old fashioned way'?",
+        choices: [ "Audi", "BMW", "Lexus", "Mercedes"],
+        correctChoice: 2
+    },
+
+    {
+        question: "Who goes missing at the Harvest Festival?",
+        choices: [ "Andy", "April", "Jerry", "Lil' Sebastian"],
+        correctChoice: 3
+    },
+
+    {
+        question: "What business did Tom Haverford start first?",
+        choices: ["Entertainment 720", "SnakeHole Lounge", "Rent-a-Swag", "Tom's Bistro"],
+        correctChoice: 0
+    },
+
+    {
+        question: "What age does Chris Traeger plan on living to?",
+        choices: ["100", "115", "120", "150"],
+        correctChoice: 3
+    },
+
+    {
+        question: "What department is the enemy of the Parks & Rec department?",
+        choices:  ["Animal Care and Services","Library", "Sanitation", "Treasury"],
+        correctChoice: 1
+    },
+      
+    {
+        question: "What is the name of Pawnee's pretentious and snotty neighbooring city?",
+        choices: ["Anderson", "Eagleton", "Indianoplois", "Bloomington" ],
+        correctChoice: 1
+    }];
+
+
+ var questionUp =0;
+ var correctChoices = 0;
+
+ 
+})
+
+/* function playtrivia(){
+        $(".triviaQ").text(trivaQuestions[i].question)
+        trivaQuestions.length; i++
+}
+
+
+
+
+// answer options going with each question will get displayed in  a b c or d  choice class //
+
+        $.each(trivaQuestions, function(){
+            $(".choice1 .label-text").html(this.choices[0])
+            $(".choice2 .label-text").html(this.choices[1])
+            $(".choice3 .label-text").html(this.choices[2])
+            $(".choice4 .label-text").html(this.choices[3])
+           
+        })
+        
+        playtrivia()
+    })
+    
+    $(".next").on('click', function nextQ() {
+       
+    })
+
+
+
+// game play//
+
+// when player clicks to start game display questions and start timer for entire game //
+
+
+// keep track of answers to keep score//
+
+//if score above certain % celebrate gif displayed//
+// if not above a good score, sad gif displayed//
+
+*/
